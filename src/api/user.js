d3b8b8d7e6da8c322efa2a88a39f4f345908134f
AyaSagili <2062195148@qq.com>
2024-05-19 17:52:52 +0800

    second

--- src/api/user.js
+++ src/api/user.js
@@ -10,7 +10,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
+    url: 'loginController/info',
     method: 'get',
     params: { token }
   })

1c2fea04da79e0148434055e61d949d661263532
AyaSagili <2062195148@qq.com>
2024-05-21 16:06:05 +0800

    llll

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -15,7 +15,7 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validUsername(str) {
-  const valid_map = ['guest']
+  const valid_map = ['guest', 'admin']
   return valid_map.indexOf(str.trim()) >= 0
 }
 
